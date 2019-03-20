c0ef30710995b441b2641219e423109d78a2be50
endtry <lucas123phan@gmail.com>
2019-03-20 15:56:53 -0700

    fix app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,13 +9,12 @@ import { FlexModalService } from './shared-components/flex-modal/flex-modal.serv
 export class AppComponent implements OnInit {
   title = 'final';
   aboutText: string;
-  constructor(private flexModal: FlexModalService) { 
+  constructor(private flexModal: FlexModalService) {
 
   }
 
   ngOnInit() {
   }
 
-  // Launch flex modal or alert (3pts reduced) showing about text
 
 }
